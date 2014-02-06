8f99d49e3eac5245200c10753b19ec599d298317
Michael Elsdörfer <michael@elsdoerfer.com>
2014-02-06 14:15:27 +0100

    closure download url changed.

--- get-closure.sh
+++ get-closure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-curl http://closure-compiler.googlecode.com/files/compiler-latest.tar.gz -o - | tar xz compiler.jar
+curl http://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz compiler.jar
 mv compiler.jar closure/closure.jar
