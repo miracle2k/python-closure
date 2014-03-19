101e4bfe0b2a4ff8454aef831f2fed53365287fd
Spencer Creasey <screasey@monetate.com>
2014-03-19 13:56:59 -0400

    fetch closure compiler from HTTPS

--- get-closure.sh
+++ get-closure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-curl http://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz compiler.jar
+curl https://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz compiler.jar
 mv compiler.jar closure/closure.jar
