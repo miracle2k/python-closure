5d3fb0c477824d70f9a64dae03464180011386a2
Michael Elsdörfer <michael@elsdoerfer.com>
2015-03-18 17:55:40 +0330

    Update version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description=open('README.rst').read(),
     author='Michael Elsdörfer',
     author_email='michael@elsdoerfer.com',
-    version="20140110",
+    version="20150315",
     url="http://pypi.python.org/pypi/closure",
     license='BSD',
     packages=find_packages(),
