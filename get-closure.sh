49a6b7c3693bf1c0ef89868a58bd4b03e12cbbb0
Michael Elsdörfer <michael@elsdoerfer.com>
2017-01-14 22:37:57 +0100

    get-closure.sh script should handle errors.

--- get-closure.sh
+++ get-closure.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+set -e
+
+rm -f closure/closure.jar
+
 curl https://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz compiler.jar
 mv compiler.jar closure/closure.jar
 
