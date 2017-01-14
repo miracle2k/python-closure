6a74444bddf680cc1bb1382bcc15742f91e8c29a
Michael Elsdörfer <michael@elsdoerfer.com>
2017-01-14 22:46:52 +0100

    Impove the get-closure script further.
    The old one failed due to closure naming the file in the archive differently.

--- get-closure.sh
+++ get-closure.sh
@@ -4,7 +4,11 @@ set -e
 
 rm -f closure/closure.jar
 
-curl https://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz compiler.jar
-mv compiler.jar closure/closure.jar
+
+DOWNLOAD_DIR="$(pwd)/download"
+mkdir -p "$DOWNLOAD_DIR"
+curl https://dl.google.com/closure-compiler/compiler-latest.tar.gz -o - | tar xz -C "$DOWNLOAD_DIR"
+mv $DOWNLOAD_DIR/*.jar closure/closure.jar
+rm -rf "$DOWNLOAD_DIR"
 
 java -jar closure/closure.jar  --version
